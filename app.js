c120fc0436dbdec167c02182a04cc24d2878433a
Abdul Rafay <abdulrafaycodeder@gmail.com>
2025-11-11 05:02:58 -0800

    complete-website

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main2'),
+//     smooth: true
+// });
 
